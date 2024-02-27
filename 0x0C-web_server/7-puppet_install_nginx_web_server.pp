5a4b96e85697c6a714ffff4cdacd7814d2252823
Dayvid0063 <orjidavid63@gmail.com>
2024-02-27 18:00:29 +0100

    pp file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,45 +1,6 @@
-# Install Nginx package via puppet
+#Install nginx
 
-# Install Nginx
-package { 'nginx':
-  ensure => installed,
-}
-
-# Ensure Nginx is listening on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-
-      root /var/www/html;
-      index index.html index.htm index.nginx-debian.html;
-
-      location / {
-        try_files \$uri \$uri/ =404;
-        add_header Content-Type text/html;
-        echo 'Hello World!';
-      }
-
-      location = /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-      }
-    }
-  ",
-  require => Package['nginx'],
-}
-
-# Activate the default Nginx configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => [File['/etc/nginx/sites-available/default'], Package['nginx']],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
