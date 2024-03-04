78bcc9c0e730e7590dbc4d8352682ab857a54e18
Dayvid0063 <orjidavid63@gmail.com>
2024-03-04 16:28:14 +0100

    created

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,23 @@
 # Install,Config with a custom header and Start Nginx
 
-exec { 'apt-get-update':
+# Update package lists
+exec { 'apt-update':
   command => '/usr/bin/apt-get update',
 }
 
 # Install Nginx package
 package { 'nginx':
   ensure  => installed,
-  require => Exec['apt-get-update'],
+  require => Exec['apt-update'],
 }
 
-# Server configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    location / {
-        try_files \$uri \$uri/ =404;
-        add_header X-Served-By \$hostname;
-    }
-    if (\$request_filename ~ redirect_me){
-        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-    }
-    error_page 404 /404.html;
-    location = /404.html {
-        root /var/www/error/;
-        internal;
-        add_header X-Served-By \$hostname;
-    }
-}",
+# Add a custom HTTP header
+file_line {'Adding_Header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => '    add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
 # Ensure Nginx service is running
