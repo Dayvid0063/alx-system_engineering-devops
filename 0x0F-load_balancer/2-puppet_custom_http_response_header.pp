035b50f7e8ec4df40171a53a8d59a66fcd6ccace
Dayvid0063 <orjidavid63@gmail.com>
2024-03-04 16:06:48 +0100

    advanced

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,43 @@
+# Install,Config with a custom header and Start Nginx
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Server configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+    location / {
+        try_files \$uri \$uri/ =404;
+        add_header X-Served-By \$hostname;
+    }
+    if (\$request_filename ~ redirect_me){
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/error/;
+        internal;
+        add_header X-Served-By \$hostname;
+    }
+}",
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+}
+
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+}
